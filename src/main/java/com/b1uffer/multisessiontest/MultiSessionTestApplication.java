1577398d012e076ca074f74e4ada3a21d1deabcc
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-10 17:34:17 +0900

    first commit

---
+++ src/main/java/com/b1uffer/multisessiontest/MultiSessionTestApplication.java
@@ -0,0 +1,13 @@
+package com.b1uffer.multisessiontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiSessionTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiSessionTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/b1uffer/multisessiontest/MultiSessionTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b1uffer.multisessiontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiSessionTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
