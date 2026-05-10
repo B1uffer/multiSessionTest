60dda11659d3e481b83a689cbad67c6298be1375
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-10 18:24:13 +0900

    InMemoryUsers 구현

--- src/main/java/com/b1uffer/multisessiontest/support/InMemoryUsers.java
+++ src/main/java/com/b1uffer/multisessiontest/support/InMemoryUsers.java
@@ -1,4 +1,30 @@
 package com.b1uffer.multisessiontest.support;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
 public class InMemoryUsers {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailService() {
+        UserDetails user = User.withUsername("user")
+                .password("{noop}password")
+                .roles("USER")
+                .build();
+
+        UserDetails user2 = User.withUsername("user")
+                .password("{noop}password")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}password")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, user2, admin);
+    }
 }
