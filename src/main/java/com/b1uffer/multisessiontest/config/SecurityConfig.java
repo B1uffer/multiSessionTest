6ecf87b4abd6b179103aea669e58532c09319ffe
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-10 19:52:09 +0900

    SecurityConfig FilterChain 구현

--- src/main/java/com/b1uffer/multisessiontest/config/SecurityConfig.java
+++ src/main/java/com/b1uffer/multisessiontest/config/SecurityConfig.java
@@ -1,4 +1,60 @@
 package com.b1uffer.multisessiontest.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    // 동시 세션 제어 핵심
+    @Bean
+    public SecurityFilterChain sessionFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login","/session-expired", "/css/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/", true)
+                        .permitAll()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                        .maximumSessions(1)
+                        .maxSessionsPreventsLogin(false)
+                        .expiredUrl("/session-expired")
+                        .sessionRegistry(sessionRegistry())
+                );
+        return http.build();
+    }
+
+//    @Bean
+//    @Order(1)
+//    public SecurityFilterChain h2FilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .securityMatcher(PathRequest.toH2Console())
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers(PathRequest.toH2Console()).permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .csrf(csrf -> csrf.disable())
+//                .headers(headers -> headers
+//                        .frameOptions(frame -> frame.sameOrigin())
+//                );
+//        return http.build();
+//    }
+
+    @Bean
+    public SessionRegistry sessionRegistry() {
+        return new SessionRegistryImpl();
+    }
 }
