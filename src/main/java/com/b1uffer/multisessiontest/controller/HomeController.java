f56ca66c76156e972b090266a03d9439c50f8c04
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-10 18:32:32 +0900

    home API 구현

--- src/main/java/com/b1uffer/multisessiontest/controller/HomeController.java
+++ src/main/java/com/b1uffer/multisessiontest/controller/HomeController.java
@@ -1,4 +1,20 @@
 package com.b1uffer.multisessiontest.controller;
 
+import jakarta.servlet.http.HttpSession;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.Authentication;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Configuration
 public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model, HttpSession session, Authentication auth) {
+        model.addAttribute("sessionId", session.getId()); // session Id
+        model.addAttribute("username", auth.getName()); // authentication name
+        model.addAttribute("creationTime", session.getCreationTime());
+        model.addAttribute("lastAccessedTime", session.getLastAccessedTime());
+        return "home";
+    }
 }
