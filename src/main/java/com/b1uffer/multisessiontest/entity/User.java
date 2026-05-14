9135c59d4aefc2f71a6c8ef8ffa382f5022811fe
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-14 14:06:44 +0900

    SecurityConfig Remember-Me 설정 사용자 데이터베이스 연동 User 엔티티 생성

---
+++ src/main/java/com/b1uffer/multisessiontest/entity/User.java
@@ -0,0 +1,26 @@
+package com.b1uffer.multisessiontest.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+}
