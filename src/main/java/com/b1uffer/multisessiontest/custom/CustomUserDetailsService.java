92a25bad3ab5e7f4235ee343f8585cd7dc2a4f64
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-14 16:54:39 +0900

    SecurityConfig Remember-Me 설정 사용자 데이터베이스 연동 import 수정

--- src/main/java/com/b1uffer/multisessiontest/custom/CustomUserDetailsService.java
+++ src/main/java/com/b1uffer/multisessiontest/custom/CustomUserDetailsService.java
@@ -3,7 +3,6 @@ package com.b1uffer.multisessiontest.custom;
 import com.b1uffer.multisessiontest.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
