2ba333f12d940728fe672c3923928b4b3180024b
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-14 16:50:43 +0900

    SecurityConfig Remember-Me 설정 사용자 데이터베이스 연동 로그 확인용, 빠진 password 로직 추가

--- src/main/java/com/b1uffer/multisessiontest/custom/CustomUserDetailsService.java
+++ src/main/java/com/b1uffer/multisessiontest/custom/CustomUserDetailsService.java
@@ -2,12 +2,15 @@ package com.b1uffer.multisessiontest.custom;
 
 import com.b1uffer.multisessiontest.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
@@ -15,8 +18,13 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("입력한 username : {}", username);
+        var users = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+
         return userRepository.findByUsername(username)
                 .map(user -> User.withUsername(user.getUsername())
+                        .password(user.getPassword())
                         .roles("USER")
                         .build())
                 .orElseThrow(() -> new UsernameNotFoundException("User not found : " + username));
