2dcec77bd4b6f952ba6d9e6bdc887b9e33b30de8
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-14 14:07:03 +0900

    SecurityConfig Remember-Me 설정 사용자 데이터베이스 연동 CustomUserDetailsService 구현

--- src/main/java/com/b1uffer/multisessiontest/service/CustomUserDetailsService.java
+++ src/main/java/com/b1uffer/multisessiontest/service/CustomUserDetailsService.java
@@ -1,4 +1,24 @@
 package com.b1uffer.multisessiontest.service;
 
-public class CustomUserDetailsService {
+import com.b1uffer.multisessiontest.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .map(user -> User.withUsername(user.getUsername())
+                        .roles("USER")
+                        .build())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found : " + username));
+    }
 }
