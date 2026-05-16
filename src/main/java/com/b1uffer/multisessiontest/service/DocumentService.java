5d923d39ec8f45300dabf3ba106a9d4666cc095f
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-16 20:04:43 +0900

    SecurityConfig 세션 기반 사용자 인가 메서드 보안을 통한 인가 주석 추가

--- src/main/java/com/b1uffer/multisessiontest/service/DocumentService.java
+++ src/main/java/com/b1uffer/multisessiontest/service/DocumentService.java
@@ -1,7 +1,21 @@
 package com.b1uffer.multisessiontest.service;
 
+import com.b1uffer.multisessiontest.entity.Document;
+import com.b1uffer.multisessiontest.repository.DocumentRepository;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DocumentService {
+    private final DocumentRepository documentRepository;
+
+    DocumentService(DocumentRepository documentRepository) {
+        this.documentRepository = documentRepository;
+    }
+
+    // 문서를 조회한 후 반환한다
+    @PostAuthorize("returnObject.owner == authentication.name")
+    public Document getDocument(Long id) {
+        return documentRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Document not found"));
+    }
 }
