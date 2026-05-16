0f6aff054690969c6cbafd52f40aa2ec31fff0cb
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-16 19:45:49 +0900

    SecurityConfig 세션 기반 사용자 인가 메서드 보안을 통한 인가 ReportService 구현

--- src/main/java/com/b1uffer/multisessiontest/service/ReportService.java
+++ src/main/java/com/b1uffer/multisessiontest/service/ReportService.java
@@ -1,8 +1,18 @@
 package com.b1uffer.multisessiontest.service;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReportService {
+    // 메서드 실행 전에 접근 권한을 확인한다
+    @PreAuthorize("hasRole('ADMIN')")
+    public String generateAdminReport() {
+        return "Admin Report";
+    }
 
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    public String generateUserReport() {
+        return "User Report";
+    }
 }
