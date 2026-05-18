5a8b1ec1b0056120943d67c2b319d82583b41046
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-18 17:57:42 +0900

    SecurityConfig 세션 기반 사용자 인가 도메인 객체 보안(Access Control List) 객체 기반 검사 hasPermission 메서드 구현

--- src/main/java/com/b1uffer/multisessiontest/service/custom/CustomPermissionEvaluator.java
+++ src/main/java/com/b1uffer/multisessiontest/service/custom/CustomPermissionEvaluator.java
@@ -1,20 +1,38 @@
 package com.b1uffer.multisessiontest.service.custom;
 
+import com.b1uffer.multisessiontest.entity.Document;
+import com.b1uffer.multisessiontest.repository.DocumentRepository;
 import org.springframework.security.access.PermissionEvaluator;
 import org.springframework.security.core.Authentication;
 
 import java.io.Serializable;
 
 public class CustomPermissionEvaluator implements PermissionEvaluator {
-    // 객체 기반 검사
+    private final DocumentRepository repository;
+
+    public CustomPermissionEvaluator(DocumentRepository repository) {
+        this.repository = repository;
+    }
+
+    // 객체 기반 검사만 하는 메서드
     @Override
-    public boolean hasPermission(Authentication authentication, Object targetDomainObject, Object permission) {
+    public boolean hasPermission(Authentication authentication,
+                                 Object targetDomainObject,
+                                 Object permission) {
+        if(targetDomainObject instanceof Document document) {
+            String username = authentication.getName();
+            return document.getOwner().equals(username);
+        }
         return false;
     }
 
     // ID 기반 검사
     @Override
-    public boolean hasPermission(Authentication authentication, Serializable targetId, String targetType, Object permission) {
+    public boolean hasPermission(Authentication authentication,
+                                 Serializable targetId,
+                                 String targetType,
+                                 Object permission) {
+
         return false;
     }
 }
